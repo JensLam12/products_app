46298673babdd8d63fa44c4140e0f62aaca2ce89
David Cabrera <jesterlynn1986@gmail.com>
2022-04-25 22:10:26 -0700

    added login desing

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:products_app/widgets/widgets.dart';
 
 class LoginScreen extends StatelessWidget {
    
@@ -6,9 +7,67 @@ class LoginScreen extends StatelessWidget {
 	
 	@override
 	Widget build(BuildContext context) {
-		return const Scaffold(
-			body: Center(
-				child: Text('Login Screen'),
+		return Scaffold(
+			body: AuthBackground( 
+				child: SingleChildScrollView(
+					child: Column(
+						children: [
+							const SizedBox(height : 220),
+							CardContainer(
+								child: Column(
+									children: [
+										const SizedBox( height: 10 ),
+										Text( 'Login', style: Theme.of(context).textTheme.headline4 ),
+										const SizedBox( height: 30 ),
+										const _LoginForm()
+									],
+								)
+							),
+							const SizedBox( height: 40 ),
+							const Text('Create a new account', style: TextStyle( fontSize: 18, fontWeight: FontWeight.bold))
+						],
+					),
+				),
+			)
+		);
+	}
+}
+
+
+class _LoginForm extends StatelessWidget {
+	const _LoginForm({Key? key}) : super(key: key);
+
+	@override
+	Widget build(BuildContext context) {
+		return Container(
+			child: Form(
+				child: Column(
+					children: [
+						TextFormField(
+							autocorrect: false,
+							keyboardType: TextInputType.emailAddress,
+							decoration: const InputDecoration(
+								enabledBorder: UnderlineInputBorder(
+									borderSide: BorderSide(
+										color: Colors.deepPurple
+									)
+								),
+								focusedBorder: UnderlineInputBorder(
+									borderSide: BorderSide(
+										color: Colors.deepPurple,
+										width: 2
+									)
+								),
+								hintText: 'example@gmail.com',
+								labelText: 'Email',
+								labelStyle: TextStyle(
+									color: Colors.grey
+								),
+								prefixIcon: Icon( Icons.alternate_email_sharp, color: Colors.deepPurple )
+							),
+						)
+					],
+				),
 			),
 		);
 	}
